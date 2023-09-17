01fa73876ca52cc104592d9f394b60125bba945b
amigin <amigin@gmail.com>
2023-09-17 13:56:36 +0300

    Fixes with generation proc_macro

--- service-sdk-macros/src/lib.rs
+++ service-sdk-macros/src/lib.rs
@@ -5,23 +5,20 @@ use proc_macro::TokenStream;
 pub fn generate_settings_signature(_item: TokenStream) -> TokenStream {
     let mut traits = vec![];
 
-    if cfg!(feature = "service-bus") {
-        traits.push(quote::quote!(+ MyServiceBusSettings));
-    }
+    traits.push(quote::quote!(+ SeqSettings));
 
-    if cfg!(feature = "no-sql-reader") {
-        traits.push(quote::quote!(+ MyNoSqlTcpConnectionSettings));
-    }
+    #[cfg(feature = "service-bus")]
+    traits.push(quote::quote!(+ MyServiceBusSettings));
 
-    if cfg!(feature = "no-sql-writer") {
-        traits.push(quote::quote!(+ MyNoSqlWriterSettings));
-    }
+    #[cfg(feature = "no-sql-reader")]
+    traits.push(quote::quote!(+ MyNoSqlTcpConnectionSettings));
+
+    #[cfg(feature = "no-sql-writer")]
+    traits.push(quote::quote!(+ MyNoSqlWriterSettings));
 
     let result = quote::quote! {
        Arc<impl MyTelemetrySettings + ServiceInfo + SeqSettings #(#traits)* + Send + Sync + 'static>
     };
-
-    // println!("Settings configuration: {}", result);
 
     result.into()
 }
