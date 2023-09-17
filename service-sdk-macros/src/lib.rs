2eeeea4260fb9a0ab1984bdb691d9540b5f4eafa
amigin <amigin@gmail.com>
2023-09-17 10:45:38 +0300

    Made macros more rust conventional way

--- service-sdk-macros/src/lib.rs
+++ service-sdk-macros/src/lib.rs
@@ -3,28 +3,22 @@ use proc_macro::TokenStream;
 
 #[proc_macro]
 pub fn generate_settings_signature(_item: TokenStream) -> TokenStream {
-    let mut signature = vec![
-        "MyTelemetrySettings",
-        "Send",
-        "Sync",
-        "ServiceInfo",
-        "SeqSettings",
-        "'static",
-    ];
+    let mut traits = vec![];
 
     if cfg!(feature = "service-bus") {
-        signature.push("MyServiceBusSettings");
+        traits.push(quote::quote!(+ MyServiceBusSettings));
     }
 
     if cfg!(feature = "no-sql-reader") {
-        signature.push("MyNoSqlTcpConnectionSettings");
+        traits.push(quote::quote!(+ MyNoSqlTcpConnectionSettings));
     }
 
     if cfg!(feature = "no-sql-writer") {
-        signature.push("MyNoSqlWriterSettings");
+        traits.push(quote::quote!(+ MyNoSqlWriterSettings));
     }
 
-    let result = format!("Arc<impl {}>", signature.join(" + ").to_string());
-
-    return result.parse().unwrap();
+    quote::quote! {
+       Arc<impl MyTelemetrySettings + ServiceInfo + SeqSettings #(#traits)* + Send + Sync + 'static>
+    }
+    .into()
 }
