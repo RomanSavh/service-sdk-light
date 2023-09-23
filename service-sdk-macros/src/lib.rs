c075a9a6cb355e64e92341ab7cb1722ff5df804e
amigin <amigin@gmail.com>
2023-09-23 17:05:52 +0300

    Added references to use_my_postgres

--- service-sdk-macros/src/lib.rs
+++ service-sdk-macros/src/lib.rs
@@ -204,6 +204,8 @@ pub fn use_my_postgres(_input: TokenStream) -> TokenStream {
         use service_sdk::my_postgres::macros::*;
         use service_sdk::my_telemetry::MyTelemetryContext;
         use service_sdk::my_logger;
+        use service_sdk::rust_extensions;
+        use service_sdk::rust_extensions::date_time::DateTimeAsMicroseconds;
     }
     .into()
 }
