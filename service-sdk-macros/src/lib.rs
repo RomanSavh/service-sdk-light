bc5fde9c7003a21fac7db536105a2847f9819070
amigin <amigin@gmail.com>
2024-01-28 01:25:47 +0200

    added use service_sdk::my_postgres::UpdateConflictType

--- service-sdk-macros/src/lib.rs
+++ service-sdk-macros/src/lib.rs
@@ -212,6 +212,7 @@ pub fn use_my_postgres(_input: TokenStream) -> TokenStream {
         use service_sdk::my_postgres::MyPostgres;
         use service_sdk::my_postgres::MyPostgresError;
         use service_sdk::my_postgres::sql_select::BulkSelectBuilder;
+        use service_sdk::my_postgres::UpdateConflictType;
         use service_sdk::my_telemetry::MyTelemetryContext;
         use service_sdk::my_logger;
         use service_sdk::rust_extensions;
